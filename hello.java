0c0ad8ce17b07a17b52b9a55d4443ec863171d86
Mayuri2506 <manjarekarmayu25@gmail.com>
2023-03-10 16:02:13 +0530

    Added Hello program

--- hello.java
+++ hello.java
@@ -1,9 +1,7 @@
 public class hello
 {
 	public static void main(String[] args){
-	System.out.println("hello,welcoome to SYIT");
-	System.out.println("This is SET practical session");
-	
-	}
-
+ 	System.out.println("Hello welcome SYIT");
+	System.out.println("This is SET practical");
+}
 }
